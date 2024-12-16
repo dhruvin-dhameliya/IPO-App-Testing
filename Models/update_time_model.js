40ae3b6f61bc0e706fbbfbf6abecffb2f6241986
Dhruvin Dhameliya <dhruvin82003@gmail.com>
2024-12-16 22:02:05 +0530

    second commit

--- Models/update_time_model.js
+++ Models/update_time_model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const updateTimeSchema = new mongoose.Schema({
     company_name: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
+    updated_at: { type: Date, default: Date.now }
 });
 
 export default mongoose.model("UpdateTime", updateTimeSchema);
